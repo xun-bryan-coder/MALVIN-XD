af73a49a577925538f4171d14a6b95d993f82e57
xun-bryan-coder <xhungbryan@gmail.com>
2025-08-06 13:21:35 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Vz8niC6L#_b-4RnlZDhkmfRcZBcG3GnoN6OGTjpDIMFQUx0-_xIA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348165820748", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Nigeria" //put your country timeZone....leave blank if u don't know.
 };
