649b79d563f5aa8f5cdb809cb5de4f0f1e054ff1
xun-bryan-coder <xhungbryan@gmail.com>
2025-08-06 17:08:49 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Vz8niC6L#_b-4RnlZDhkmfRcZBcG3GnoN6OGTjpDIMFQUx0-_xIA",  // add your Session Id here
+  SESSION_ID: "malvin~QvdU1JzS#02GIsOCRrlYBZxjrm1846rPz4GD9FhMh_tHP7VX0tF8",  // add your Session Id here
   
-  OWNER_NUMBER: "2348165820748", // put your phone number here
+  OWNER_NUMBER: "2348103283453", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
